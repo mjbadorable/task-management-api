105c22ddcd72883147dc8f5d6f5c09275165e98a
Marvin <adorablemarvinjhay@gmail.com>
2026-03-12 14:37:14 +0800

    Implement database CRUD endpoints

--- app/crud.py
+++ app/crud.py
@@ -1,8 +1,8 @@
 from sqlalchemy.orm import Session
 from . import models
 
-def get_tasks(db:Session):
-    return db.query(models.Task).all()
+def get_tasks(db:Session, task_id: int):
+    return db.query(models.Task).filter(models.Task.id == task_id).first()
 
 def create_task(db: Session, title: str):
     task = models.Task(title=title)
@@ -13,6 +13,20 @@ def create_task(db: Session, title: str):
 
 def delete_task(db: Session, task_id: int):
     task = db.query(models.Task).filter(models.Task.id == task_id).first()
+
     if task:
         db.delete(task)
         db.commit()
+
+    return  task
+
+def update_task(db:Session, task_id: int, title: str, completed: bool):
+    task = db.query(models.Task).filter(models.Task.id == task_id).first()
+
+    if task:
+        task.title = title
+        task.completed = completed
+        db.commit()
+        db.refresh(task)
+
+    return task
--- app/routers/tasks.py
+++ app/routers/tasks.py
@@ -11,11 +11,19 @@ def get_db():
     finally:
         db.close() # Always close after response
 
-@router.get("/tasks")
-def read_tasks(db: Session = Depends(get_db)):
-    return crud.get_tasks(db)
+@router.get("/tasks/{task_id}")
+def read_tasks(task_id: int, db: Session = Depends(get_db)):
+    return crud.get_tasks(db, task_id)
 
-@router.post("/tasks")
+@router.post("/tasks", response_model=schemas.Task)
 def create_task(task: schemas.TaskCreate, db: Session = Depends(get_db)):
     return crud.create_task(db, task.title)
+
+@router.put("/tasks/{task_id}")
+def update_task(task_id: int, title: str, completed: bool, db: Session = Depends(get_db)):
+    return crud.update_task(db, task_id, title, completed)
+
+@router.delete("/tasks/{task_id}")
+def delete_task(task_id: int, db: Session = Depends(get_db)):
+    return crud.delete_task(db, task_id)
 
